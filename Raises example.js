6f716b40636d99d9b873671dde0584a0e20f4cf3
ACraig33 <92490439+ACraig33@users.noreply.github.com>
2022-03-28 13:18:33 -0400

    Add files via upload

---
+++ Raises example.js
@@ -0,0 +1,14 @@
+function main() {
+    var increase = parseInt(readLine(), 10);
+    var prices= [98.99, 15.2, 20, 1026];
+    
+	
+   var i;
+   var newPrices=[];
+   for(i=0;i<=3;i++){
+    prices.length;
+    prices[i]=prices[i]+increase;
+   }
+    console.log(prices);
+
+}
